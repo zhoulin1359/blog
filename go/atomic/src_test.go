12e89104b5c34db978538c6eafc0276341f0056d
zhoulin <zhoulin@jingyu.com>
2022-11-29 19:50:21 +0800

    context 1.17

--- go/atomic/src_test.go
+++ go/atomic/src_test.go
@@ -6,6 +6,8 @@ func TestAdd(t *testing.T) {
 	t.Log(Add(1000))
 }
 
+//https://amazingao.com/posts/2020/11/go-src/sync/atomic/
+//https://golang.design/under-the-hood/zh-cn/part1basic/ch01basic/asm/
 func TestCompareAndSwap(t *testing.T) {
 	if 1 != CompareAndSwap(1000) {
 		t.Fatal("err")
